7a0d63f1856e4635356999916c516398bf3c7f45
Mihai Dragomir <msdragomir@yahoo.ro>
2020-06-21 10:30:50 +0300

    Cosmetic updates

--- ro_interview_assignment/loggin_module.py
+++ ro_interview_assignment/loggin_module.py
@@ -6,15 +6,18 @@ def get_logger(logger_name, level):
     Create logger with the specified name, configure the logger level
     and console handler
     """
+    # Set logging level
+    logging_level = getattr(logging, level, logging.NOTSET)
+
     # Create logger
     logger = logging.getLogger(logger_name)
 
     # Set logger level
-    logger.setLevel(getattr(logging, level, logging.NOTSET))
+    logger.setLevel(logging_level)
 
     # Configure console handler
     console_handler = logging.StreamHandler()
-    console_handler.setLevel(logging.DEBUG)
+    console_handler.setLevel(logging_level)
 
     # Create formatter and add it to the handler
     console_handler_formatter = logging.Formatter(
